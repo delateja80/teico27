9126aa6e96598660ab957bcf6b49fb32fad06939
CristianFu <134558440+CristianFu@users.noreply.github.com>
2023-10-10 10:16:06 -0600

    Update App.java

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Hola mundo 2");
-        System.out.println ("Amonooooos);
+        System.out.println ("Amonooooos");
     }
 }
