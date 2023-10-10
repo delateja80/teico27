a50fc9af16b8f2a5422525b0c2beb99f6ef79c25
fredyquintana <141890829+fredyquintana@users.noreply.github.com>
2023-10-10 10:20:58 -0600

    CAMBIE TEXTO

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("Hola mundo 2");
+        System.out.println("Hola mundo ICO 27");
         System.out.println ("Amonooooos");
     }
 }
