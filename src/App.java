d7085b6578afdf21eef88fb85a60418cd591926a
LuisE <luiscv2enrique@gmail.com>
2023-10-10 10:21:37 -0600

    hola

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Comiendo taquitos");
-        System.out.println("Hola");
+        System.out.println("Hola2");
     }
 }
